d7b8a138f0180a5be3238adc5cd16db6c9ad7008
nach <nachnitror@gmail.com>
2023-04-21 08:48:34 +0200

    ADD links route on navbar

--- StarWars/src/Components/Navbar.jsx
+++ StarWars/src/Components/Navbar.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../assets/Styled-Navbar.css";
 
 const Navbar = () => {
+
   return (
     <>
       <div className="Navbar">
-        <h3 className="h3Navbar">HOME</h3>
-        <h3 className="h3Navbar">STARSHIPS</h3>
+        <Link to="/HomePage">
+          <h3 className="h3Navbar">
+            HOME
+          </h3>
+        </Link>
+
+        <Link to="/StarshipPage">
+          <h3 className="h3Navbar">
+            STARSHIPS
+          </h3>
+        </Link>
       </div>
     </>
   );
