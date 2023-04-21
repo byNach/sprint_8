e6cf0eeeef4965b5cc5efb914040692c41f59a44
nach <nachnitror@gmail.com>
2023-04-21 08:42:11 +0200

    ADD pages

---
+++ StarWars/src/Pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Header from "../Components/Header";
+import Navbar from "../Components/Navbar";
+import Wellcome from "../Components/Wellcome";
+
+const HomePage = () => {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <Wellcome />
+    </>
+  );
+};
+
+export default HomePage;
---
+++ StarWars/src/Pages/StarshipPage.jsx
@@ -0,0 +1,15 @@
+import Header from "../Components/Header";
+import Navbar from "../Components/Navbar";
+import GetStarship from "../Components/GetApiStarships";
+
+const StarshipPage = () => {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <GetStarship />
+    </>
+  );
+};
+
+export default StarshipPage;
