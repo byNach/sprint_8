f9f174decfe6f7e075837df6982f714911a9a1bc
nach <nachnitror@gmail.com>
2023-04-23 09:32:52 +0200

    CREATE singUp Page

---
+++ StarWars/src/Pages/SingUpPage.jsx
@@ -0,0 +1,15 @@
+import Header from "../Components/Header";
+import Navbar from "../Components/Navbar";
+import SingUp from "../Components/Login";
+
+const SingUpPage = () => {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <SingUp />
+    </>
+  );
+};
+
+export default SingUpPage;
