7629b7ba6725fe9ef21ed291106f36847037c87d
nach <nachnitror@gmail.com>
2023-04-23 09:32:32 +0200

    CREATE logIn page

---
+++ StarWars/src/Pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+import Header from "../Components/Header";
+import Navbar from "../Components/Navbar";
+import Login from "../Components/Login";
+
+const LoginPage = () => {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <Login />
+    </>
+  );
+};
+
+export default LoginPage;
