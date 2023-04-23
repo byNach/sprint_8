33b8cbc65f9da26d9e88033d525a2377d7b77f9f
nach <nachnitror@gmail.com>
2023-04-23 16:42:13 +0200

    CREATE singUP logic and render

--- StarWars/src/Components/SingUp.jsx
+++ StarWars/src/Components/SingUp.jsx
@@ -1,38 +1,101 @@
 import "../assets/Styled-SingUp.css";
-import { useState } from "react";
+import { useState} from "react";
 
 const SingUp = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  
+  const [userName, setUserName] = useState("");
+  const [newPassword, setPassword] = useState("");
+  const [repeatedPassword, setRepeatedPassword] = useState("");
+  const [errorNameMessage, setErrorNameMessage] = useState("");
+  const [errorPasswordMessage, setErrorPasswordMessage] = useState("");
+  const [userNameValid, setUserNameValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
+ 
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const isUserNameValid = userName !== "";
+    const doPasswordsMatch =
+      (newPassword === repeatedPassword && newPassword) ||
+      repeatedPassword !== "";
+    !isUserNameValid
+      ? setErrorNameMessage("Error: Username not valid!") &
+        setUserNameValid(false)
+      : setErrorNameMessage("") & setUserNameValid(true);
+    !doPasswordsMatch
+      ? setErrorPasswordMessage("Error: Passwords must match!") &
+        setPasswordValid(false)
+      : setErrorPasswordMessage("") & setPasswordValid(true);
+  };
+
+
+  const saveUserCredentials = () => {
+    localStorage.setItem("username", userName);
+    localStorage.setItem("password", newPassword);
+  };
+
   return (
     <form>
       <div className="login">
-        <div className="login-screen">         
-          <div className="login-form">
-            <div className="control-group">
-              <input
-                type="text"
-                className="login-field"
-                placeholder="username"
-                id="login-name"
-              ></input>
-              <label className="login-field-icon fui-user"></label>
+        <div className="login-screen">
+          {userNameValid & passwordValid ? (
+               <div className="LoginSuccesBase">
+               {saveUserCredentials()}
+               <p className="LoginSucces">Hello {userName},</p>
+               <p className="LoginSucces">You joined the force!</p>
+               <iframe
+                 src="https://giphy.com/embed/AcfTF7tyikWyroP0x7"
+                 width="480"
+                 height="270"
+                 className="giphy-embed"
+                 allowFullScreen
+               ></iframe>
+             </div>
+                ) : (
+            <div className="login-form">
+              <div className="control-group">
+                <input
+                  type="text"
+                  className="login-field"
+                  placeholder="username"
+                  id="login-name"
+                  value={userName}
+                  onChange={(e) => setUserName(e.target.value)}
+                />
+                <label className="login-field-icon fui-user"></label>
+              </div>
+              <div className="control-group">
+                <input
+                  type="newPassword"
+                  className="login-field"
+                  placeholder="Password"
+                  id="login-pass"
+                  value={newPassword}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <label className="login-field-icon fui-lock"></label>
+              </div>
+              <div className="control-group">
+                <input
+                  type="newPassword"
+                  className="login-field"
+                  placeholder="repeat Password"
+                  id="login-repeated-pass"
+                  value={repeatedPassword}
+                  onChange={(e) => setRepeatedPassword(e.target.value)}
+                />
+                <label className="login-field-icon fui-lock"></label>
+              </div>
+              <a className="btn" href="#" onClick={handleSignUp}>
+                <p className="LoginP">SING UP</p>
+              </a>
+              {errorNameMessage ? (
+                <p className="ErrorMessage">{errorNameMessage}</p>
+              ) : null}
+              {errorPasswordMessage ? (
+                <p className="ErrorMessage">{errorPasswordMessage}</p>
+              ) : null}
             </div>
-            <div className="control-group">
-              <input
-                type="current-password"
-                className="login-field"
-                placeholder="password"
-                id="login-pass"
-              ></input>
-              <label className="login-field-icon fui-lock"></label>
-            </div>
-            <a className="btn" href="#">
-              <h5>Hello</h5>
-              <p className="LoginP">SING UP</p>
-            </a>
-          </div>
+         
+          )}
         </div>
       </div>
     </form>
