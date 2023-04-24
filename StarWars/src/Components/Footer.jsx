87201b8bfed667eeada3f95d6876ea6625fd7876
nach <nachnitror@gmail.com>
2023-04-24 12:19:56 +0200

    CREATE footer component

---
+++ StarWars/src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import "../assets/Styled-Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="Footer">
+        <h4 className="Footer-text">TM & © Lucasfilm Ltd. All Rights Reserved</h4>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
