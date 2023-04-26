be49b579d09ef1ddd0db89128362ad41acb19696
nach <nachnitror@gmail.com>
2023-04-26 10:39:58 +0200

    CREATE LogOutPage

---
+++ StarWars/src/Pages/LogOutPage.jsx
@@ -0,0 +1,17 @@
+import Header from "../Components/Header";
+import Navbar from "../Components/Navbar";
+import LogOut from "../Components/LogOut";
+import Footer from "../Components/Footer";
+
+const LogOutPage = () => {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <LogOut />
+      <Footer />
+    </>
+  );
+};
+
+export default LogOutPage;
