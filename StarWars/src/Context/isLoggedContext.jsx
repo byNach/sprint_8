e1727d72b638234c71ebd295d41ed72bc4db9cf3
nach <nachnitror@gmail.com>
2023-04-28 10:58:41 +0200

    ADD functions changeLoggedTrue & changeLoggedFalse

--- StarWars/src/Context/isLoggedContext.jsx
+++ StarWars/src/Context/isLoggedContext.jsx
@@ -1,19 +1,24 @@
-import { createContext, useState } from 'react';
+import { createContext, useState } from "react";
 
 export const IsLoggedContext = createContext();
 
 const IsLoggedProvider = ({ children }) => {
   const [logged, setLogged] = useState(false);
 
-  const changeLogged = () => {
-    setLogged(logged === true ? false : true);
+  const changeLoggedTrue = () => {
+    setLogged(true);
+  };
+  const changeLoggedFalse = () => {
+    setLogged(false);
   };
 
   return (
-    <IsLoggedContext.Provider value={{logged, changeLogged}}>
+    <IsLoggedContext.Provider
+      value={{ logged, changeLoggedTrue, changeLoggedFalse }}
+    >
       {children}
     </IsLoggedContext.Provider>
-  )
-}
+  );
+};
 
 export default IsLoggedProvider;
